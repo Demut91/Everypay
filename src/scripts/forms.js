066ce90168119a120b3356fe9c6e321f1810408e
root <root@DESKTOP-IONV53U.localdomain>
2021-08-07 15:17:21 +0800

    message script added

--- src/scripts/forms.js
+++ src/scripts/forms.js
@@ -1,36 +1,39 @@
+import show from "./message";
+
 const forms = () => {
-  const apiurl = 'http://ptsv2.com/t/0rft5-1628159972/post';
-  const form = document.querySelectorAll ('form');
-  const inputs = document.querySelectorAll ('input, textarea');
-
-  const postData = async (url, data) => {
-    let res = await fetch (url, {
-      method: 'POST',
-      body: data,
+    const apiurl = 'http://ptsv2.com/t/0rft5-1628159972/post';
+    const form = document.querySelectorAll ('form');
+    const inputs = document.querySelectorAll ('input, textarea');
+  
+    const postData = async (url, data) => {
+      let res = await fetch (url, {
+        method: 'POST',
+        body: data,
+      });
+      return await res.text ();
+    };
+  
+    const clearInputs = () => {
+      inputs.forEach (item => {
+        item.value = '';
+      });
+    };
+  
+    form.forEach (item => {
+      item.addEventListener ('submit', e => {
+        show ();
+        e.preventDefault ();
+  
+        const formData = new FormData (item);
+        postData (apiurl, formData)
+          .then (res => {
+            console.log (res);
+          })
+          .finally (() => {
+            clearInputs ();
+          });
+      });
     });
-    return await res.text ();
   };
-
-  const clearInputs = () => {
-    inputs.forEach (item => {
-      item.value = '';
-    });
-  };
-
-  form.forEach (item => {
-    item.addEventListener ('submit', e => {
-      e.preventDefault ();
-
-      const formData = new FormData (item);
-      postData (apiurl, formData)
-        .then (res => {
-          console.log (res);
-        })
-        .finally (() => {
-          clearInputs ();
-        });
-    });
-  });
-};
-
-export default forms;
+  
+  export default forms;
