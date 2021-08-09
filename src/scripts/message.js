deab96a727a556293baf6ecb26b74d5bb844dcd7
root <root@DESKTOP-IONV53U.localdomain>
2021-08-09 12:30:05 +0800

    prefix added

--- src/scripts/message.js
+++ src/scripts/message.js
@@ -4,17 +4,26 @@ function show () {
   mess.innerText = 'Проверьте e-mail!';
   let place = document.querySelectorAll ('.smallform');
 
+  const bigmess = document.createElement('div')
+  bigmess.className = 'message-big'
+  bigmess.innerHTML = 'Спасибо! <p>Мы приняли ваш запрос.</p><p>В ближайшее время наш специалист поддержки свяжется с вами.</p>'
+  let bigplace = document.querySelector('.footer-form')
+  bigplace.appendChild(bigmess)
+
   for (let i = 0; i < place.length; i++) {
     place[i].appendChild (mess.cloneNode (true));
   }
-
-  setTimeout (hide, 5000);
+  
+  setTimeout (hide, 4000);  
 
   function hide () {
     for (let i = 0; i < document.querySelectorAll ('.message-small').length; i++) {
-      document.querySelectorAll ('.message-small')[i].style.visibility = 'hidden';
-      //     document.getElementsByClassName('message-small')[i].remove();
+      document.querySelectorAll ('.message-small')[i].style.visibility = 'hidden';     
     }
+    for (let i = 0; i < document.querySelectorAll ('.message-small').length; i++) {     
+      document.getElementsByClassName('message-small')[i].remove();
+    }       
+    bigplace.removeChild(bigmess)
   }
 }
 
